2641e0df85ca51d9badc714f54ca3119f91e8e19
Krongkarn Jitsil <poopan@taskworld.com>
2017-09-13 18:36:24 +0700

    :hammer::hammer::hammer: try to use vector approach

--- test/createMower.spec.js
+++ test/createMower.spec.js
@@ -6,13 +6,12 @@ const expect = Lab.expect
 const createMower = require('../src/createMower')
 
 lab.experiment('createMower', () => {
-
   function getMower () {
     return createMower({
-      upperRightCorner: { x: 5, y: 5  },
+      upperRightCorner: [5, 5],
       initialPosition: {
-        coordinate: { x: 1, y: 2 },
-        direction: 'N'
+        coordinate: [1, 2],
+        direction: [0, 1]
       }
     })
   }
@@ -20,48 +19,48 @@ lab.experiment('createMower', () => {
   lab.test(
     'return object contains initial position with the given position', (done) => {
       expect(getMower().position).to.equal({
-        coordinate: { x: 1, y: 2 },
-        direction: 'N'
+        coordinate: [1, 2],
+        direction: [0, 1]
       })
       done()
     }
   )
 
   describe('move', () => {
-    it ('should rotate 90 degree left when given input `L`', (done) => {
+    it('should rotate 90 degree left when given input `L`', (done) => {
       const mower = getMower()
       mower.move('L')
       expect(mower.position).to.equal({
-        coordinate: { x: 1, y: 2 },
-        direction: 'W'
+        coordinate: [1, 2],
+        direction: [-1, 0]
       })
       done()
     })
 
-    it ('should rotate back to origin input `L` 4 times', (done) => {
+    it('should rotate back to origin input `L` 4 times', (done) => {
       const mower = getMower()
       mower.move('L')
       mower.move('L')
       mower.move('L')
       mower.move('L')
       expect(mower.position).to.equal({
-        coordinate: { x: 1, y: 2 },
-        direction: 'N'
+        coordinate: [1, 2],
+        direction: [0, 1]
       })
       done()
     })
 
-    it ('should rotate 90 degree right when given input `R`', (done) => {
+    it('should rotate 90 degree right when given input `R`', (done) => {
       const mower = getMower()
       mower.move('R')
       expect(mower.position).to.equal({
-        coordinate: { x: 1, y: 2 },
-        direction: 'E'
+        coordinate: [1, 2],
+        direction: [1, 0]
       })
       done()
     })
 
-    it ('should rotate back to clock-wise given input `R` 7 times', (done) => {
+    it('should rotate back to clock-wise given input `R` 7 times', (done) => {
       const mower = getMower()
       mower.move('R')
       mower.move('R')
@@ -71,24 +70,24 @@ lab.experiment('createMower', () => {
       mower.move('R')
       mower.move('R')
       expect(mower.position).to.equal({
-        coordinate: { x: 1, y: 2 },
-        direction: 'W'
+        coordinate: [1, 2],
+        direction: [-1, 0]
       })
       done()
     })
 
-    it ('should move towards the direction when given input `F`', (done) => {
+    it('should move towards the direction when given input `F`', (done) => {
       const mower = getMower()
       mower.move('R')
       mower.move('F')
       expect(mower.position).to.equal({
-        coordinate: { x: 2, y: 2 },
-        direction: 'E'
+        coordinate: [2, 2],
+        direction: [1, 0]
       })
       done()
     })
 
-    it ('should not move if the position after the move is outside the lawn', (done) => {
+    it('should not move if the position after the move is outside the lawn', (done) => {
       const mower1 = getMower()
       mower1.move('L')
       mower1.move('F')
@@ -100,8 +99,8 @@ lab.experiment('createMower', () => {
       mower1.move('F')
       mower1.move('F')
       expect(mower1.position).to.equal({
-        coordinate: { x: 1, y: 0 },
-        direction: 'S'
+        coordinate: [1, 0],
+        direction: [0, -1]
       })
 
       const mower2 = getMower()
@@ -119,8 +118,8 @@ lab.experiment('createMower', () => {
       mower2.move('F')
       mower2.move('F')
       expect(mower2.position).to.equal({
-        coordinate: { x: 5, y: 4 },
-        direction: 'E'
+        coordinate: [5, 4],
+        direction: [1, 0]
       })
       done()
     })
